949354b97758153bdb87d069cfe21fe1198ff5c9
SpringNyan <springnyan@gmail.com>
2018-04-15 15:09:41 +0800

    Fix space indentation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./lib/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "vue-typescript-inject.js",
-        library: "vue-typescript-inject",
-        libraryTarget: "umd"
-    },
-    externals: ["vue", "vue-class-component"]
+  mode: "development",
+  entry: "./lib/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "vue-typescript-inject.js",
+    library: "vue-typescript-inject",
+    libraryTarget: "umd"
+  },
+  externals: ["vue", "vue-class-component"]
 };
